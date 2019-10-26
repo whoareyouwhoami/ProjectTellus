411ed5afda3b8f79eefeea1f405ce7075dd49693
YW mac15 <young.woo@hotmail.com>
2019-10-26 22:21:08 +0900

    new app setting configured

--- SW4DS_django/SW4DS_django/urls.py
+++ SW4DS_django/SW4DS_django/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-
+    path('', include('kickstart.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
