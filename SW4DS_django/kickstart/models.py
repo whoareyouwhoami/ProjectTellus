5d729572c357e504f2ebbae75a22d608034594e1
YW mac15 <young.woo@hotmail.com>
2019-11-06 00:23:21 +0900

    WEB v.2.1 db models added

--- SW4DS_django/kickstart/models.py
+++ SW4DS_django/kickstart/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+## Main category choice
+mcat_choices = [('', '---------'),('Art', 'Art'), ('Comics', 'Comics'), ('Crafts', 'Crafts'), ('Dance', 'Dance'),
+               ('Design', 'Design'), ('Fashion', 'Fashion'), ('Film & Video', 'Film & Video'), ('Food', 'Food'),
+               ('Games', 'Games'), ('Journalism', 'Journalism'), ('Music', 'Music'), ('Photography', 'Photography'),
+               ('Publishing', 'Publishing'), ('Technology', 'Technology'), ('Theater', 'Theater')]
+## Currency choice
+currency_choices = [('', '---------'), ('AUD', 'AUD'), ('CAD', 'CAD'), ('CHF', 'CHF'), ('DKK', 'DKK'), ('EUR', 'EUR'),
+                   ('GBP', 'GBP'), ('HKD', 'HKD'), ('JPY', 'JPY'), ('MXN', 'MXN'), ('NOK', 'NOK'), ('NZD', 'NZD'),
+                   ('SEK', 'SEK'), ('SGD', 'SGD'), ('USD', 'USD')]
+
+## Country choice
+country_choices = [('', '---------'), ('Australia', 'Australia'), ('Austria', 'Austria'), ('Belgium', 'Belgium'),
+                   ('Canada', 'Canada'), ('Denmark', 'Denmark'), ('France', 'France'), ('Germany', 'Germany'),
+                   ('Hong Kong', 'Hong Kong'), ('Ireland', 'Ireland'), ('Italy', 'Italy'), ('Japan', 'Japan'),
+                   ('Luxembourg', 'Luxembourg'), ('Mexico', 'Mexico'), ('New Zealand', 'New Zealand'),
+                   ('Norway', 'Norway'), ('Singapore', 'Singapore'), ('Spain', 'Spain'), ('Sweden', 'Sweden'),
+                   ('Switzerland', 'Switzerland'), ('the Neterlands', 'the Neterlands'),
+                   ('the United Kingdom', 'the United Kingdom'), ('the United States', 'the United States')]
+
+
+class DBform(models.Model):
+    search_id = models.AutoField(primary_key=True)
+    main_category = models.CharField(max_length=100,choices=mcat_choices)
+    category = models.CharField(max_length=100)
+    location = models.CharField(max_length=100,choices=country_choices)
+    currency = models.CharField(max_length=100,choices=currency_choices)
+    date_start = models.DateField()
+    date_end = models.DateField()
+    goal = models.PositiveIntegerField()
+
