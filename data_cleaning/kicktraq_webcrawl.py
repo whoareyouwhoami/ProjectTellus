61350a6499d51cd4a37ad014dfd7e9952f132b1e
YW mac15 <young.woo@hotmail.com>
2019-11-23 14:40:47 +0900

    new kicktraq webcrawl v.1

--- data_cleaning/kicktraq_webcrawl.py
+++ data_cleaning/kicktraq_webcrawl.py
@@ -8,6 +8,7 @@ import time
 import pandas as pd
 import numpy as np
 import requests
+from datetime import datetime
 from selenium import webdriver as wd
 from selenium.webdriver.common.keys import Keys
 
@@ -28,6 +29,171 @@ success_currency = {'AU$': 'AUD', 'CA$': 'CAD', 'HK$': 'HKD', 'MX$': 'MXN', 'NZ$
 unsuccess_currency = {'Australia': 'AUD', 'Canada': 'CAD', 'Hong Kong': 'HKD', 'Mexico': 'MXN', 'NZ': 'NZD', 'Singapore': 'SGD'}
 
 currfix = ['£','€','CHF','¥']
+
+
+
+class KicktraqOpen:
+    def __init__(self, url):
+        self.url = url
+        self.driver = wd.Chrome(path + '/chromedriver')
+        self.driver.get(self.url)
+
+class KicktraqPage(KicktraqOpen):
+    def __init__(self):
+        super().__init__("https://www.kicktraq.com/projects/")
+
+    def getdayone(self):
+        self.dayonepage = "https://www.kicktraq.com/dayones/"
+        # self.driver.get(self.dayonepage)
+        return self.dayonepage
+
+    def getarchive(self):
+        self.archivepage = "https://www.kicktraq.com/archive/"
+        # self.driver.get(self.archivepage)
+        return self.archivepage
+
+    def getInfoSuccess(self, text, attrurl):
+        self.text = text
+        self.attrurl = attrurl
+
+        backers = int(self.text.split("\n")[0].split(": ")[1])
+        pledged = self.text.split('\n')[1].split(': ')[1].split(' of ')[0]
+        goal = self.text.split('\n')[1].split(': ')[1].split(' of ')[1].split(' (')[0]
+        year = self.text.split('\n')[3].split('(')[1][:-1]
+        launched = self.text.split('\n')[3].split(": ")[1].split(' -> ')[0]
+        deadline = self.text.split('\n')[3].split(": ")[1].split(' -> ')[1].split(' (')[0]
+
+        launched = year + ' ' + launched
+        deadline = year + ' ' + deadline
+
+        currency_t = goal[0]
+
+        if currency_t not in currfix:
+            currency_t = get_currency_type(currency_t, self.attrurl)
+
+
+        res_dct_pass = {'state':'success',
+                        'backers':backers,
+                        'pledged': pledged,
+                        'goal': goal,
+                        'launched': launched,
+                        'deadline': deadline,
+                        'currency_type':currency_t}
+
+        return res_dct_pass
+        # row = [name, blurb, 'success', category, backers, percent, pledged, goal, year + ' ' + launched,
+        #        year + ' ' + deadline]
+        # blurb_df = blurb_df.append(pd.Series(row,
+        #                                      index=['name', 'blurb', 'state', 'category', 'backers', 'funding_rate',
+        #                                             'pledged', 'goal', 'launched', 'deadline']), ignore_index=True)
+        #
+
+    def getInfoFail(self, text, attrurl):
+        self.text = text
+        self.attrurl = attrurl
+
+        backers = int(self.text.split("\n")[0].split(": ")[1])
+        pledged = self.text.split('\n')[1].split(': ')[1].split(' of ')[0]
+        goal = self.text.split('\n')[1].split(': ')[1].split(' of ')[1].split(' (')[0]
+        year = self.text.split('\n')[2].split('(')[1][:-1]
+        launched = self.text.split('\n')[2].split(": ")[1].split(' -> ')[0]
+        deadline = self.text.split('\n')[2].split(": ")[1].split(' -> ')[1].split(' (')[0]
+
+        launched = year + ' ' + launched
+        deadline = year + ' ' + deadline
+
+        currency_t = goal[0]
+
+        if currency_t not in currfix:
+            currency_t = get_currency_type(currency_t, self.attrurl)
+
+        res_dct_fail ={'state':'fail',
+                       'backers': backers,
+                       'pledged': pledged,
+                       'goal': goal,
+                       'launched': launched,
+                       'deadline': deadline,
+                       'currency_type': currency_t}
+
+        return res_dct_fail
+        # row = [name, blurb, 'failed', category, backers, percent, pledged, goal, year + ' ' + launched,
+        #        year + ' ' + deadline]
+        # blurb_df = blurb_df.append(pd.Series(row,
+        #                                      index=['name', 'blurb', 'state', 'category', 'backers', 'funding_rate',
+        #                                             'pledged', 'goal', 'launched', 'deadline']), ignore_index=True)
+
+
+class KicktraqCrawl(KicktraqPage):
+    def __init__(self):
+        super().__init__()
+
+    def webcrawl(self, start, end, text):
+        self.start = start
+        self.end = end
+        self.text = text
+
+        current_date = datetime.date(datetime.now())
+
+        if self.text == "dayone":
+            self.openpg = super().getdayone()
+        elif self.text == "archive":
+            self.openpg = super().getarchive()
+        else:
+            return "Either `dayone` or `archive` page available"
+
+        for page in range(self.start, self.end + 1):
+            print("==================================")
+            print("Collecting page", page, "......")
+            print("==================================")
+
+            self.driver.get('{}?page={}'.format(self.openpg, page))
+
+            ## COMMON
+            prj_list = self.driver.find_elements_by_xpath("//div[@class='project-infobox']")
+            prj_length = len(prj_list)
+
+            for i in range(prj_length):
+                print(">> Collecting prioject",i,"right now...")
+                x = prj_list[i]
+                p1 = x.find_element_by_xpath("h2/a")
+                detail_url = p1.get_attribute("href")
+
+                ## TITLE
+                name = p1.text
+
+                ## BLURB
+                blurb = x.find_element_by_xpath("div[not(@class)]").text
+
+                # if blurb is not empty
+                if blurb != "":
+                    ## CATEGORY
+                    cat = x.find_element_by_xpath("div[@class='project-cat']")
+                    cat_lst = cat.find_elements_by_tag_name("a")
+                    category = cat_lst[0].text
+
+                    ## PROJECT INFORMATION
+                    info = x.find_element_by_xpath("div[@class='project-infobits']/div[@class='project-details']")
+                    prj_info = info.text
+
+                    # finding rate
+                    rate_info = info.find_element_by_tag_name("span").text
+                    percent = int(re.findall('\d+', rate_info)[0])
+
+                    if percent >= 100 & self.text == 'dayone':
+                        collect = super().getInfoSuccess(prj_info, detail_url)
+
+                    elif percent < 100 & self.text == 'archive':
+                        collect = super().getInfoFail(prj_info, detail_url)
+
+                    main_dct = {'collected_date': current_date,
+                                'name': name,
+                                'blurb': blurb,
+                                'category': category,
+                                'funding_rate': percent}
+
+
+kick = KicktraqCrawl()
+kick.getdayone()
 
 def get_currency_type(currency_symb, detail_url):
     global currency_type
@@ -128,71 +294,3 @@ def get_currency_type(currency_symb, detail_url):
         new_driver.close()
     driverx.close()
     return currency_type
-
-
-def webcrawl(start, end):
-    timestart = time.time()
-    driver = wd.Chrome(path + '/chromedriver')
-    global blurb_df
-    for page in range(start,end+1):
-        print("==================================")
-        print("Collecting page",page,"......")
-        print("==================================")
-
-        # load page
-        driver.get('https://www.kicktraq.com/dayones/?page={}'.format(page))
-
-        # get list of all project list and info
-        prj_list = driver.find_elements_by_xpath("//div[@class='project-infobox']")
-        prj_length = len(prj_list)
-
-        for i in range(prj_length):
-            print(">> Collecting project",i+1,"right now...")
-            x = prj_list[i]
-            p1 = x.find_element_by_xpath("h2/a")
-
-            ## TITLE
-            name = p1.text
-
-            ## BLURB
-            blurb = x.find_element_by_xpath("div[not(@class)]").text
-
-            ## CATEGORY
-            cat =  x.find_element_by_xpath("div[@class='project-cat']")
-            cat_lst = cat.find_elements_by_tag_name("a")
-            category = cat_lst[0].text
-
-            ## PROJECT INFORMATION
-            info = x.find_element_by_xpath("div[@class='project-infobits']/div[@class='project-details']")
-            prj_info = info.text
-
-            # finding rate
-            rate_info = info.find_element_by_tag_name("span").text
-            percent = int(re.findall('\d+', rate_info)[0])
-
-            if percent >= 100:
-                pledged = prj_info.split('\n')[1].split(': ')[1].split(' of ')[0]
-                goal = prj_info.split('\n')[1].split(': ')[1].split(' of ')[1].split(' (')[0]
-                year = prj_info.split('\n')[3].split('(')[1][:-1]
-                launched = prj_info.split('\n')[3].split(": ")[1].split(' -> ')[0]
-                deadline = prj_info.split('\n')[3].split(": ")[1].split(' -> ')[1].split(' (')[0]
-
-
-                # finding currency
-                currency_t = goal[0]
-
-                if currency_t not in currfix:
-                    detail_url = p1.get_attribute("href")
-                    currency_t = get_currency_type(currency_t, detail_url)
-
-                row = [name, blurb, 'success', category, percent, pledged, goal, currency_t, year + ' ' + launched,
-                       year + ' ' + deadline]
-
-
-                blurb_df = blurb_df.append(pd.Series(row, index=['name', 'blurb', 'state', 'category', 'funding_rate', 'pledged', 'goal','currency_type', 'launched', 'deadline']), ignore_index=True)
-
-    driver.close()
-    print("==================================")
-    print("!COMPLETED!")
-    print("==================================")
-    print("Completed in {} seconds...".format(round(time.time()-timestart),2))
