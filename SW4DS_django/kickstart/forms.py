7d542b84b421821942965c995e87acd3f520510b
YW mac15 <young.woo@hotmail.com>
2019-12-01 20:45:09 +0900

    WEB v.3.1

--- SW4DS_django/kickstart/forms.py
+++ SW4DS_django/kickstart/forms.py
@@ -2,13 +2,14 @@ from django import forms
 from django.forms import ModelForm
 from . import models
 from django.utils.translation import gettext_lazy as _
+from datetime import date, datetime
 # Year choice
 year_choices = [x for x in range(2019,2031)]
 
 class checkform(ModelForm):
     class Meta:
         model = models.DBform
-        exclude = ['search_id']
+        exclude = ['search_id', 'amount_usd','bin_term','bin_goal']
         labels = {
             'main_category':_('Main category'),
             'location':_('Country'),
